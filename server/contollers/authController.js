ddf342207b63062d1e2f396c54dc21f4b3b55f79
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-24 23:54:21 +0300

    server validation Relate #26

--- server/contollers/authController.js
+++ server/contollers/authController.js
@@ -1,44 +1,53 @@
 const Joi = require('joi');
 const { loginQuery, signUpQuery } = require('../database/quieres');
-const schema = require('../util/schema');
+const { loginSchema, signupSchema } = require('../util/schema');
 
 const login = (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // server side validation
-    Joi.validateAsync({ username, password }, schema).then((result) => console.log(result)).catch((err) => {
-        console.log(err);
-        res.json("your credentials does not match our records");
+  // server side validation
+  Joi.validateAsync({ username, password }, loginSchema)
+    .then((result) => console.log(result))
+    .catch((err) => {
+      console.log(err);
+      res.json('your credentials does not match our records');
     });
 
-    loginQuery({ username, password })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch(console.log);
+  loginQuery({ username, password })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(console.log);
 };
 
 const signUp = (req, res) => {
-    const {
-        username,
-        email,
-        password,
-        confirmdPassword,
-    } = req.body;
+  const {
+    username, email, password, confirmdPassword,
+  } = req.body;
 
-    // server side validation
-
-    signUpQuery({
-            username,
-            email,
-            password,
-            confirmdPassword,
-        })
-        .then(console.log)
-        .catch(console.log);
+  // server side validation
+  Joi.validateAsync(
+    {
+      username, password, confirmdPassword, email,
+    },
+    signupSchema,
+  )
+    .then((result) => console.log(result))
+    .catch((err) => {
+      console.log(err);
+      res.json('your credentials does not match our records');
+    });
+  signUpQuery({
+    username,
+    email,
+    password,
+    confirmdPassword,
+  })
+    .then(console.log)
+    .catch(console.log);
 };
 
 module.exports = {
-    login,
-    signUp,
+  login,
+  signUp,
 };
