6b242ef5a4a654d7f3a0a5034732e98b83621b51
Nizar7zak <nizar7zakout@gmail.com>
2021-08-25 01:21:08 +0300

    solve conflicts and handle endpoints for login and sign up page

--- server/router.js
+++ server/router.js
@@ -5,15 +5,19 @@ const router = express.Router();
 const errorController = require('./contollers/errorController');
 const authController = require('./contollers/authController');
 const tasksController = require('./contollers/tasksController');
-
-router.post('/login', authController.login);
-router.post('/sign-up', authController.signUp);
+const loginPage = require('./contollers/loginPageController');
+const signUpPage = require('./contollers/signUpPageController');
 
 router.get('/tasks', tasksController.getTasks);
 router.post('/tasks', tasksController.insertTask);
 router.put('/tasks/:taskId/done', tasksController.taskDone);
+router.get('/login', loginPage);
+router.get('/signUp', signUpPage);
 
+router.post('/login', authController.login);
+router.post('/sign-up', authController.signUp);
 router.use(errorController.NotFound);
 router.use(errorController.ServerError);
+
 
 module.exports = router;
