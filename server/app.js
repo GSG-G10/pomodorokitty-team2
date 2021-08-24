b13e01adbd88b8d14ab267dba6db5c0d85c1fc55
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-24 15:25:11 +0300

    create server Relates #3

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require('env2')('.env');
+const express = require('express');
+const compresion = require('compression');
+const { join } = require('path');
+const router = require('./router');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 7000);
+app.disable('x-powered-by');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compresion());
+app.use(express.static(join(__dirname, '..', 'public')));
+
+app.use(router);
+
+module.exports = app;
