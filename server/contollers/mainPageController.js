988efcd5376cbc71cd6c6e4371569bf5e8e48f02
Nizar7zak <nizar7zakout@gmail.com>
2021-08-26 15:22:19 +0300

    solve heroku

--- server/contollers/mainPageController.js
+++ server/contollers/mainPageController.js
@@ -2,7 +2,7 @@ const { join } = require('path');
 
 const mainPage = (req, res) => {
   if (req.cookies.accessToken) {
-    res.sendFile(join(__dirname, '..', 'public', 'pamdor.html'));
+    res.sendFile(join(__dirname, '..', '..', 'public', 'pamdor.html'));
   } else {
     res.redirect('/login');
   }
